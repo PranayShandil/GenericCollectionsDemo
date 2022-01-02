bfac98ad54bc62e067d3a14826bbdd4432c4fd13
Pranay Shandil <pranay.shandil@mercell.com>
2022-01-02 11:01:04 +0100

    Generic collection initial commit

---
+++ GenericCollectionsBasic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollectionsDemo
+{
+    class GenericCollectionsBasic
+    {
+        static void Main()
+        {
+            List<int> li = new List<int>();
+            li.Add(10);
+            li.Add(20);
+            li.Add(30);
+            li.Add(40);
+            li.Add(50);
+            li.Add(60);
+
+            foreach (int i in li)
+                Console.Write(i + " ");
+            Console.WriteLine();
+
+            for (int i = 0; i < li.Count; i++)
+            {
+                Console.WriteLine(li[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
